605e41603ff22ae919ab9bd1e59370bdd8ea81a4
Cailloux7789 <matteocayol3@gmail.com>
2023-04-03 22:17:33 +0200

    rewrite explain

--- projet irrigation/pompe.ino
+++ projet irrigation/pompe.ino
@@ -1,14 +1,13 @@
-void gestion_pompe(char etat) //appelle fonction permettant de demarrer la pompe lorsque le sol est sec
+void gestion_pompe(char etat) // définit une fonction prenant en entrée un caractère etat
 {
- if(etat==0) 
- { 
-     digitalWrite(LED_verte_pompe, HIGH); 
-     digitalWrite(POMPE, HIGH); 
- } 
- else 
- { 
-     digitalWrite(LED_verte_pompe, LOW); 
-     digitalWrite(POMPE, LOW);  
- } 
+    if (etat == 0) // si l'état est 0 (le sol est sec)
+    {
+        digitalWrite(LED_verte_pompe, HIGH); // allume la LED verte de la pompe
+        digitalWrite(POMPE, HIGH); // active la pompe
+    }
+    else // sinon (le sol est humide)
+    {
+        digitalWrite(LED_verte_pompe, LOW); // éteint la LED verte de la pompe
+        digitalWrite(POMPE, LOW); // désactive la pompe
+    }
 }
-
