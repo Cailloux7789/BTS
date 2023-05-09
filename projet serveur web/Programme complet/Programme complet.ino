a9271422a43c1e8fdce7ad67d0f28774c9659ddf
Cailloux <115746001+Cailloux7789@users.noreply.github.com>
2023-05-09 12:52:47 +0200

    page web

--- projet serveur web/Programme complet/Programme complet.ino
+++ projet serveur web/Programme complet/Programme complet.ino
@@ -1,9 +1,7 @@
 #include <WiFi.h>
-#include <WiFiClient.h>
-#include <WebServer.h>
 
-const char* ssid = "";      // Entrez le nom du réseau Wi-Fi
-const char* password = "";  // Entrez le mot de passe du réseau Wi-Fi
+const char* ssid = "snec";      // Entrez le nom du réseau Wi-Fi
+const char* password = "btssnjeanperrin";  // Entrez le mot de passe du réseau Wi-Fi
 
 WiFiServer server(80);
 
@@ -17,12 +15,11 @@ void setup() {
   Serial.begin(115200);
   pinMode(output26, OUTPUT);
   pinMode(output27, OUTPUT);
-  digitalWrite(output26, LOW);
-  digitalWrite(output27, LOW);
+  digitalWrite(output26, HIGH);
+  digitalWrite(output27, HIGH);
 
   Serial.print("Connecting to ");
   Serial.println(ssid);
-
   WiFi.begin(ssid, password);
 
   while (WiFi.status() != WL_CONNECTED) {
@@ -42,46 +39,46 @@ void loop() {
   WiFiClient client = server.available();  // Listen for incoming clients
   if (client) {                            // If a new client connects,
     Serial.println("New Client.");         // print a message out in the serial port
-    String currentLine = "";               // make a String to hold incoming data from the client
+      String currentLine = "";               // make a String to hold incoming data from the client
     while (client.connected()) {           // loop while the client's connected
       if (client.available()) {            // if there's bytes to read from the client,
         char c = client.read();            // read a byte, then
         Serial.write(c);                   // print it out the serial monitor
         header += c;
-        if (c == '\n') {  // if the byte is a newline character
-          // if the current line is blank, you got two newline characters in a row.
-          // that's the end of the client HTTP request, so send a response:
+        if (c == '\n') {
           if (currentLine.length() == 0) {
-            // HTTP headers always start with a response code (e.g. HTTP/1.1 200 OK)
-            // and a content-type so the client knows what's coming, then a blank line:
             client.println("HTTP/1.1 200 OK");
             client.println("Content-type:text/html");
             client.println("Connection: close");
             client.println();
-
+          }
+          break;
             // turns the GPIOs on and off
             if (header.indexOf("GET /26/on") >= 0) {
               Serial.println("GPIO 26 on");
               output26State = "on";
               digitalWrite(output26, HIGH);
-            } else if (header.indexOf("GET /26/off") >= 0) {
+            }
+            else if (header.indexOf("GET /26/off") >= 0) {
               Serial.println("GPIO 26 off");
               output26State = "off";
               digitalWrite(output26, LOW);
-            } else if (header.indexOf("GET /27/on") >= 0) {
+            }
+            else if (header.indexOf("GET /27/on") >= 0) {
               Serial.println("GPIO 27 on");
               output27State = "on";
               digitalWrite(output27, HIGH);
-            } else if (header.indexOf("GET /27/off") >= 0) {
+            }
+            else if (header.indexOf("GET /27/off") >= 0) {
               Serial.println("GPIO 27 off");
               output27State = "off";
               digitalWrite(output27, LOW);
             }
-          }
+          
         }
       }
     }
-  }
+  
 
   client.println("<!DOCTYPE html><html>");
   client.println("<head><meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">");
@@ -107,4 +104,5 @@ void loop() {
   output26State = digitalRead(output26) == HIGH ? "on" : "off";
   output27State = digitalRead(output27) == HIGH ? "on" : "off";
   client.stop();
+  }
 }
